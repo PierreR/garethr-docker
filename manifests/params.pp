2aaa04426120fdd175583895578280f8dbf4e09d
Jean-Francois Roche <jfroche@affinitic.be>
2016-03-15 15:13:26 +0100

    Revert "Initialize varialbe $service_overrides_template"
    This reverts commit d804e992dc546702eba895be7136f4e221459914.

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,7 +22,6 @@ class docker::params {
   $service_state                     = running
   $service_enable                    = true
   $service_provider                  = undef
-  $service_overrides_template        = undef
   $manage_service                    = true
   $root_dir                          = undef
   $tmp_dir                           = '/tmp/'
